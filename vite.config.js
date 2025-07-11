79aac70ac3a3f7fe7901b76dea44f0255e14bfc3
i4rcu <abdurrahmanyos23@gmail.com>
2025-07-11 18:51:22 +0300

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace 'portfolio' with your actual GitHub repository name
-export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio/',
-})
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/', // Replace 'portfolio' with your actual GitHub repository name
 })
